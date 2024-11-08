63a855b0aab2757ffb5c0569b0f7f6aab60e2645
sophiagatinha <162448760+sophiagatinha@users.noreply.github.com>
2024-11-08 11:33:16 -0300

    Add files via upload

---
+++ Minha ferramenta de estudos_sophia/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Github',
+    'O Github é um repositório de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão?',
+    'A capital do Japão é Tokio'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz mesa em Inglês?',
+    'Mesa em ingles é table (TEIBOU)'
+)
